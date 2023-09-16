cb1d8ef751dcd244024bd0b7bf02a8057b715e7a
Nidash <nidash@gmail.com>
2023-09-16 15:37:06 +0100

    Use useReducer instead of useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+//import React, { useState } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  //const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(prevCount => prevCount + 1);
+    //setCount(prevCount => prevCount + 1);
+    dispatch({ type: 'increment' });
   }
 
   function decrement() {
-    setCount(prevCount => prevCount - 1);
+    //setCount(prevCount => prevCount - 1);
+    dispatch({ type: 'decrement' });
   }
-  
+
   return (
     <div>
       <button onClick={decrement}>-</button>
-      <span>{count}</span>
+      <span>{state.count}</span>
       <button onClick={increment}>+</button>
     </div>
   );
